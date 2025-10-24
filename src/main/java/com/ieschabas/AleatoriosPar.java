557a67f8fd8fc3c15200780c7ad37efc5eba9e2e
Kiri <fffggg5055@gmail.com>
2025-10-24 18:01:11 +0200

    aleatoriopar fix

--- src/main/java/com/ieschabas/AleatoriosPar.java
+++ src/main/java/com/ieschabas/AleatoriosPar.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class AleatoriosPar {
     static void main() {
+        Random rng = new Random();
         Scanner scanner = new Scanner(System.in);
         int limite = 0;
         while (true) {
@@ -19,16 +20,16 @@ public class AleatoriosPar {
             }
         }
         AleatoriosPar aleatoriosPar = new AleatoriosPar();
-        System.out.println(aleatoriosPar.parAleatorioMenorQue(limite,null));
+        System.out.println(aleatoriosPar.parAleatorioMenorQue(limite,rng));
     }
 
     public int parAleatorioMenorQue(int limite, Random rng) {
         int numero = 0;
-        while (numero % 2 == 1) {
-
+        while (true) {
             numero = rng.nextInt(0, limite);
-
-
+            if (numero %2 == 0){
+                break;
+            }
         }
         return numero;
 
